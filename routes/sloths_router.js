44baa90bc44a299e25b9d62c7116455435fe83b8
bharding2 <b.harding@gmail.com>
2016-04-22 14:18:09 -0700

    put and delete

--- routes/sloths_router.js
+++ routes/sloths_router.js
@@ -17,3 +17,32 @@ slothsRouter.get('/sloths', function* () {
     this.response.body = data;
   });
 });
+
+slothsRouter.get('/sloths/:id', function* () {
+  yield Sloth.findOne({ _id: this.params.id }, (err, data) => {
+    if (err) return handleErr(err, this.response.body);
+    this.response.body = data;
+  });
+});
+
+slothsRouter.put('/sloths/:id', koaBody, function* () {
+  var slothData = this.request.body;
+
+  yield Sloth.findOne({ _id: this.params.id }, (err, doc) => {
+    if (err) return handleErr(err, this.response.body);
+    doc.name = slothData.name;
+    doc.toes = slothData.toes;
+    doc.strength = slothData.strength;
+    doc.save(() => {
+      this.response.body = 'sloth updated';
+    });
+  });
+
+});
+
+slothsRouter.del('/sloths/:id', function* () {
+  yield Sloth.remove({ _id: this.params.id }, (err) => {
+    if (err) return handleErr(err, this.response.body);
+    this.response.body = 'sloth removed';
+  });
+});
